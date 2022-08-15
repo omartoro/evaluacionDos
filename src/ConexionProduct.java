79ab21f64182db0eac69dc24bb9d16c42d61d517
ItsgabrielJT <gabrielasimbaya82@gmail,com>
2022-08-15 15:31:31 -0500

    Agregando funciones CRUD

--- src/ConexionProduct.java
+++ src/ConexionProduct.java
@@ -42,12 +42,34 @@ public class ConexionProduct
 
     public void insetarDatos(String nombre, String cantidad, String precio) {
         try {
-            PreparedStatement insert = conexion.prepareStatement("INSERT INTO users VALUES(?, ?, ?, ?)");
+            PreparedStatement insert = conexion.prepareStatement("INSERT INTO productos VALUES(?, ?, ?, ?)");
             insert.setString(1, null);
             insert.setString(2, nombre);
             insert.setString(3, cantidad);
             insert.setString(4, precio);
             insert.executeUpdate();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void eliminarDatos(String id) {
+        try {
+            PreparedStatement eliminar = conexion.prepareStatement("DELETE from productos WHERE id = ?");
+            eliminar.setString(1, id);
+            eliminar.executeUpdate();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void actualizarDatos(String nombre, String cantidad, String precio, String id) {
+        try {
+            PreparedStatement modificar = conexion.prepareStatement("UPDATE productos SET nombre = ?, cantidad = ?, precio = ? WHERE id =  " + id);
+            modificar.setString(1, nombre);
+            modificar.setString(2, cantidad);
+            modificar.setString(3, precio);
+            modificar.executeUpdate();
         }catch (Exception e) {
             e.printStackTrace();
         }
--- src/farmaciaCrud.java
+++ src/farmaciaCrud.java
@@ -1,9 +1,11 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 
 public class farmaciaCrud extends JFrame
 {
+    private ConexionProduct conn = ConexionProduct.getInstancia();
     private JPanel mainPanel;
     private JTextField nombreTF;
     private JTextField IdTF;
@@ -13,6 +15,13 @@ public class farmaciaCrud extends JFrame
     private JButton actualizarButton;
     private JButton borrarButton;
     private JLabel nombre;
+
+    private void limpiarPantalla() {
+        cantidadTF.setText("");
+        nombreTF.setText("");
+        precioTF.setText("");
+        IdTF.setText("");
+    }
 
     public farmaciaCrud(JFrame parent) {
         setTitle("MegaMarketyt");
@@ -26,7 +35,49 @@ public class farmaciaCrud extends JFrame
                 String nombre = nombreTF.getText();
                 String cantidad = cantidadTF.getText();
                 String precio = precioTF.getText();
+                try {
+                    conn.conectarBasedeDatos();
+                    conn.insetarDatos(nombre, cantidad, precio);
+                    System.out.println("InSerccion Exitosa");;
+                    conn.cerrarBasedeDatos();
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+                limpiarPantalla();
+            }
+        });
 
+        borrarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String id = IdTF.getText();
+                try {
+                    conn.conectarBasedeDatos();
+                    conn.eliminarDatos(id);
+                    System.out.println("Elimnacion Exitosa");;
+                    conn.cerrarBasedeDatos();
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+                limpiarPantalla();
+            }
+        });
+        actualizarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String id = IdTF.getText();
+                String nombre = nombreTF.getText();
+                String cantidad = cantidadTF.getText();
+                String precio = precioTF.getText();
+                try {
+                    conn.conectarBasedeDatos();
+                    conn.actualizarDatos(nombre, cantidad, precio, id);
+                    System.out.println("Modificacion Exitosa");;
+                    conn.cerrarBasedeDatos();
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+                limpiarPantalla();
             }
         });
     }
